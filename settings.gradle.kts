e5817f7181df225ba7cc76f27ae56f5e39760b6b
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2024-01-07 15:19:13 +0200

    Fix indentation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,30 +1,30 @@
 pluginManagement {
-	repositories {
-		maven("https://maven.fabricmc.net/") {
-			name = "Fabric"
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
+    repositories {
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 fun getProp(name: String): String {
-	return extra.properties[name] as String
+    return extra.properties[name] as String
 }
 
 dependencyResolutionManagement {
-	versionCatalogs.create("libs") {
-		library("fabric-api", "net.fabricmc.fabric-api:fabric-api:${getProp("fabric_version")}")
-		library("fabric-loader", "net.fabricmc:fabric-loader:${getProp("loader_version")}")
-		library("fabric-yarn", "net.fabricmc:yarn:${getProp("yarn_mappings")}")
-		library("minecraft", "com.mojang:minecraft:${getProp("minecraft_version")}")
+    versionCatalogs.create("libs") {
+        library("fabric-api", "net.fabricmc.fabric-api:fabric-api:${getProp("fabric_version")}")
+        library("fabric-loader", "net.fabricmc:fabric-loader:${getProp("loader_version")}")
+        library("fabric-yarn", "net.fabricmc:yarn:${getProp("yarn_mappings")}")
+        library("minecraft", "com.mojang:minecraft:${getProp("minecraft_version")}")
 
-		library("rei-api", "me.shedaniel:RoughlyEnoughItems-api-fabric:${getProp("rei_version")}")
-		library("rei-plugin-default", "me.shedaniel:RoughlyEnoughItems-default-plugin-fabric:${getProp("rei_version")}")
-		library("rei-all", "me.shedaniel:RoughlyEnoughItems-fabric:${getProp("rei_version")}")
-		library("rei-architectury", "dev.architectury:architectury-fabric:${getProp("architectury_version")}")
-		library("rei-math", "me.shedaniel.cloth:basic-math:${getProp("cloth_basic_math_version")}")
-		library("rei-config", "me.shedaniel.cloth:cloth-config-fabric:${getProp("cloth_config_version")}")
-		library("emi", "dev.emi:emi-fabric:${getProp("emi_version")}")
-	}
+        library("rei-api", "me.shedaniel:RoughlyEnoughItems-api-fabric:${getProp("rei_version")}")
+        library("rei-plugin-default", "me.shedaniel:RoughlyEnoughItems-default-plugin-fabric:${getProp("rei_version")}")
+        library("rei-all", "me.shedaniel:RoughlyEnoughItems-fabric:${getProp("rei_version")}")
+        library("rei-architectury", "dev.architectury:architectury-fabric:${getProp("architectury_version")}")
+        library("rei-math", "me.shedaniel.cloth:basic-math:${getProp("cloth_basic_math_version")}")
+        library("rei-config", "me.shedaniel.cloth:cloth-config-fabric:${getProp("cloth_config_version")}")
+        library("emi", "dev.emi:emi-fabric:${getProp("emi_version")}")
+    }
 }
--- src/client/java/io/github/mattidragon/tlaapi/impl/TlaApi.java
+++ src/client/java/io/github/mattidragon/tlaapi/impl/TlaApi.java
@@ -9,15 +9,15 @@ import org.slf4j.LoggerFactory;
 public class TlaApi {
     public static final Logger LOGGER = LoggerFactory.getLogger("TLA Api");
 
-	public static void loadPlugins(PluginContext implementation) {
-		var entrypoints = FabricLoader.getInstance().getEntrypoints("tla-api", TlaApiPlugin.class);
-		LOGGER.info("Loading {} plugins for {}", entrypoints.size(), implementation);
+    public static void loadPlugins(PluginContext implementation) {
+        var entrypoints = FabricLoader.getInstance().getEntrypoints("tla-api", TlaApiPlugin.class);
+        LOGGER.info("Loading {} plugins for {}", entrypoints.size(), implementation);
         for (var entrypoint : entrypoints) {
-		 	try {
-				entrypoint.register(implementation);
-			} catch (RuntimeException e) {
-				 throw new RuntimeException("Error while handling tla api plugin " + entrypoint, e);
-			}
+             try {
+                entrypoint.register(implementation);
+            } catch (RuntimeException e) {
+                 throw new RuntimeException("Error while handling tla api plugin " + entrypoint, e);
+            }
         }
     }
 }
