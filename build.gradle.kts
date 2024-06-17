37aed2c9124348391ade3278cc0c7c3f8f3de968
Sollace <sollacea@gmail.com>
2024-06-17 14:09:40 +0100

    Update to emi release

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ group = "io.github.mattidragon"
 base.archivesName = "TLA-Api"
 
 repositories {
-    flatDir { dirs("lib") }
     maven("https://maven.shedaniel.me/")
     maven("https://maven.terraformersmc.com/")
 }
